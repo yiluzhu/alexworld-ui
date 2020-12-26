81e790be35e16a44a329c09626ed7252313291ff
Yilu Zhu <zhuyilu@hotmail.com>
2020-12-26 22:07:57 +0000

    update backend url

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
 
-export var rootUrl = process.env.BACKEND_URL;
+export var rootUrl = 'https://05kwx69y2f.execute-api.eu-west-2.amazonaws.com/api/';
 // export var rootUrl = 'http://localhost:5000';
 console.log('backend url is', rootUrl)
