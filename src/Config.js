83e0c5a42ce617432f18138964a567c44171a486
Yilu Zhu <zhuyilu@hotmail.com>
2020-12-26 22:25:41 +0000

    add version

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
 
 export var rootUrl = 'https://05kwx69y2f.execute-api.eu-west-2.amazonaws.com/api/';
 // export var rootUrl = 'http://localhost:5000';
-console.log('backend url is', rootUrl)
+console.log('Version is 1.0.0. Backend url is', rootUrl)
