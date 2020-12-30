1daf1c0eba2bc17454c5b4746f9b9e687cf682ce
Yilu Zhu <zhuyilu@hotmail.com>
2020-12-30 10:16:53 +0000

    fix ads

--- src/components/Question.js
+++ src/components/Question.js
@@ -109,7 +109,7 @@ export default class Question extends Component {
         <Link to='/'>
           <div className='text-right mt'>START AGAIN</div>
         </Link>
-        <div><Ad /></div>
+        {/* <div><Ad /></div> */}
         <h5 className='mt'>Difficulty: { this.state.difficulty }</h5>
         <h5 className='mt'>Number: { this.state.number }</h5>
         <div className='text-center'>
